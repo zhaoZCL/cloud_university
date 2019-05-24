fcfad7a45d6814b94e43d76d44a8f3315e32be09
lht <liu_ht@126.com>
2019-05-24 08:22:52 +0800

    update the config information and support eureka cluster

--- teacher-service/src/main/java/com/chinasoft/teacher/Application.java
+++ teacher-service/src/main/java/com/chinasoft/teacher/Application.java
@@ -2,6 +2,7 @@ package com.chinasoft.teacher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by VerRan.Liu on 2017/9/18.
